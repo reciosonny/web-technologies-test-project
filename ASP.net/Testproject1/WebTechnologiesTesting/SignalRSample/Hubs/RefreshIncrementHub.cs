304c5a2f2f9d165ca3adc92553ac385cd3faecc3
Sonny Recio <reciosonny@gmail.com>
2016-11-09 23:36:49 +0800

    added another sample for SignalR

---
+++ ASP.net/Testproject1/WebTechnologiesTesting/SignalRSample/Hubs/RefreshIncrementHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRSample.Hubs {
+    public class RefreshIncrementHub : Hub {
+
+        private static int count = 0;
+        public void IncrementCount() {
+            count += 1;
+            Clients.All.getCount(count);
+        }
+
+
+    }
+}
